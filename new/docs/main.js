c0fa350da770b99d4b1db5be4d92fc448507179d
Paul Brzeski <mail@paulbrzeski.com>
2022-10-15 17:15:27 +0800

    Implemented Three JS based building with texture filters

--- new/docs/main.js
+++ new/docs/main.js
@@ -1091,34 +1091,120 @@
 
     function init() {
 
-        
-        new THREE.Scene();
-        new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        var canvasElm = document.querySelector('canvas.overlay');
+        var scrollWidth = Math.max(
+            document.body.clientWidth, document.documentElement.clientWidth
+        );
+        var scrollHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
 
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
+        // Setup renderer.
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, scrollWidth / scrollHeight, 0.1, 1000);
+        camera.position.z = 800;
 
-        var elem = document.getElementById("myDiv");
-        var style = window.getComputedStyle(elem);
 
-        var originalTop = style.marginTop.replace('px', '');
+        var renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            antialias: true,
+            canvas: canvasElm
+        });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(scrollWidth, scrollHeight);
+
+        // Setup scene core.
+
+        var ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+        scene.add(ambientLight);
+
+        var pointLight = new THREE.PointLight( 0xffffff, 0.5 );
+        camera.add( pointLight );
+
+        scene.add(camera);
+
+        // Setup scene objects.
+        var map = new THREE.TextureLoader().load('assets/page1_hq_building.png');
+        map.anisotropy = 16;
+        map.wrapS = map.wrapT = THREE.RepeatWrapping;
+        //map.minFilter = map.magFilter = THREE.LinearFilter;
+
+        var material = new THREE.MeshPhongMaterial({ map: map, side: THREE.DoubleSide, transparent: true });
+        var object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 4, 4), material);
+        var scale = 5.75;
+        object.scale.set(scale, scale, scale);
+        var buildingPosition = -300;
+
+        object.position.set(0, buildingPosition, 0);
+        scene.add(object);
 
         window.scene = {
-            'animations': {
-                'building': animate({
-                    onUpdate: function (latest) {
-                        document.getElementById("myDiv").style.marginTop = (parseFloat(originalTop) + latest) + "px";
-                    },
-                    repeat: Infinity,
-                    to: [5, -5, 0],
-                    duration: 5000,
-                })
+            animations: {
+                building: {
+                    yPosition: buildingPosition,
+                    tween: animate({
+                        duration: 5000,
+                        onUpdate: function (latest) { return window.scene.animations.building.yPosition = buildingPosition + latest; },
+                        repeat: Infinity,
+                        to: [5, -5, 0],
+                    })
+                }            
             }
         };
         
-        console.log('The page successfully loaded!');
 
+        animateScene();
+
+        window.addEventListener( 'resize', onWindowResize );
+
+        function onWindowResize() {
+            var scrollWidth = Math.max(
+                document.body.clientWidth, document.documentElement.clientWidth
+            );
+
+            var scrollHeight = Math.max(
+                document.body.scrollHeight, document.documentElement.scrollHeight,
+                document.body.offsetHeight, document.documentElement.offsetHeight,
+                document.body.clientHeight, document.documentElement.clientHeight
+            );
+
+            camera.aspect = scrollWidth / scrollHeight;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize( scrollWidth, scrollHeight );
+
+        }
+
+        function animateScene() {
+
+            window.requestAnimationFrame(animateScene);
+
+            render();
+        }
+
+        function render() {
+
+            var timer = Date.now() * 0.0001;
+
+            camera.lookAt(scene.position);
+
+            scene.traverse(function (object) {
+
+                if (object.isMesh === true) {
+
+                    object.position.y += Math.cos(timer) * 0.025;
+
+                }
+
+            });
+
+            renderer.render(scene, camera);
+
+        }
+
+        console.log('The page successfully loaded!');
     }
 
     docReady(init);
--- new/src/scripts/main.js
+++ new/src/scripts/main.js
@@ -14,53 +14,120 @@ function docReady(fn) {
 
 function init() {
 
-    
+    const canvasElm = document.querySelector('canvas.overlay');
+    const scrollWidth = Math.max(
+        document.body.clientWidth, document.documentElement.clientWidth
+    );
+    const scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+
+    // Setup renderer.
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const camera = new THREE.PerspectiveCamera(75, scrollWidth / scrollHeight, 0.1, 1000);
+    camera.position.z = 800;
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
 
-    var elem = document.getElementById("myDiv");
-    var style = window.getComputedStyle(elem);
+    const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true,
+        canvas: canvasElm
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(scrollWidth, scrollHeight);
 
-    let originalTop = style.marginTop.replace('px', '');
+    // Setup scene core.
+
+    const ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+    scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight( 0xffffff, 0.5 );
+    camera.add( pointLight );
+
+    scene.add(camera);
+
+    // Setup scene objects.
+    const map = new THREE.TextureLoader().load('assets/page1_hq_building.png');
+    map.anisotropy = 16;
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    //map.minFilter = map.magFilter = THREE.LinearFilter;
+
+    const material = new THREE.MeshPhongMaterial({ map: map, side: THREE.DoubleSide, transparent: true });
+    let object = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 4, 4), material);
+    let scale = 5.75;
+    object.scale.set(scale, scale, scale);
+    let buildingPosition = -300;
+
+    object.position.set(0, buildingPosition, 0);
+    scene.add(object);
 
     window.scene = {
-        'animations': {
-            'building': animate({
-                onUpdate: latest => {
-                    document.getElementById("myDiv").style.marginTop = (parseFloat(originalTop) + latest) + "px";
-                },
-                repeat: Infinity,
-                to: [5, -5, 0],
-                duration: 5000,
-            })
+        animations: {
+            building: {
+                yPosition: buildingPosition,
+                tween: animate({
+                    duration: 5000,
+                    onUpdate: latest => window.scene.animations.building.yPosition = buildingPosition + latest,
+                    repeat: Infinity,
+                    to: [5, -5, 0],
+                })
+            }            
         }
     };
     
-    console.log('The page successfully loaded!');
 
-    let time = 0;
-    
-    //animateScene(time);
+    animateScene();
 
-    function animateScene(time) {
-        let newTime = parseInt(time) + 1;
+    window.addEventListener( 'resize', onWindowResize );
 
-        var elem = document.getElementById("myDiv");
-        var style = window.getComputedStyle(elem);
+    function onWindowResize() {
+        const scrollWidth = Math.max(
+            document.body.clientWidth, document.documentElement.clientWidth
+        );
 
-        let originalTop = style.marginTop.replace('px', '');
+        const scrollHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
 
-        let change = window.scene.animations.building;
+        camera.aspect = scrollWidth / scrollHeight;
+        camera.updateProjectionMatrix();
 
-        document.getElementById("myDiv").style.marginTop = (parseFloat(currentTop) + change) + "px";
-        time = newTime;
-        window.requestAnimationFrame(animateScene)
+        renderer.setSize( scrollWidth, scrollHeight );
+
     }
 
+    function animateScene() {
+
+        window.requestAnimationFrame(animateScene);
+
+        render();
+    }
+
+    function render() {
+
+        const timer = Date.now() * 0.0001;
+
+        camera.lookAt(scene.position);
+
+        scene.traverse(function (object) {
+
+            if (object.isMesh === true) {
+
+                object.position.y += Math.cos(timer) * 0.025;
+
+            }
+
+        });
+
+        renderer.render(scene, camera);
+
+    }
+
+    console.log('The page successfully loaded!');
 }
 
 docReady(init);
