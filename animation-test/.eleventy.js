2aad009d4350b1769556ceb248dd4e6e24f86cfb
Paul Brzeski <mail@paulbrzeski.com>
2023-06-24 16:59:41 +0800

    - Moving animation test to its own folder - Fixing footer html

---
+++ animation-test/.eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.min.js": "./alpine.js",
+  });
+
+  return {
+    dir: {
+      input: 'src/pages',
+      data: '../data',
+      output: 'docs'
+    },
+  };
+};
